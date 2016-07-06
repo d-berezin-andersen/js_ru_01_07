9f95370acd9120cc32f9053f8dbd66657fb4cf89
Denis Berezin <d.berezin@andersenlab.com>
2016-07-06 22:18:27 +0300

    added state for comments list

--- src/CommentsList.js
+++ src/CommentsList.js
@@ -1,29 +1,50 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Comment from './Comment'
 
 class CommentsList extends Component {
+    state = {
+        isOpen: false
+    }
 
     render() {
-        const { comments = [] } = this.props
-        const listItems = comments.map((comment) => <li key = {comment.id}><Comment comment = {comment}/></li>)
-        
-        if (!listItems.length) {
+
+        const {isOpen} = this.state
+
+        if (!isOpen) {
             return (
                 <div>
-                    <h4>Comments list</h4>
-                    There are no comments so far! Be the first one!
+                    <a href="#" onClick={this.toggleOpen}> Show comments </a>
                 </div>
             )
         }
-        
-        return (
+
+        const {comments = []} = this.props
+
+        if (!comments.length) {
+            return (
+                <div>
+                    <a href="#" onClick={this.toggleOpen}> Hide comments </a>
+                    <p>There are no comments so far! Be the first one!</p>
+                </div>
+            )
+        }
+
+        const listItems = comments.map((comment) => <li key={comment.id}><Comment comment={comment}/></li>)
+return (
             <div>
-                <h4>Comments list</h4>
+                <a href="#" onClick={this.toggleOpen}> Hide comments </a>
                 <ul>
                     {listItems}
                 </ul>
             </div>
         )
+    }
+
+    toggleOpen = (e) => {
+        e.preventDefault();
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     }
 }
 
